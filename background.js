6e58ff8d40155b3b6b34692b93bfda132830aade
galulex <galulex@gmail.com>
2022-12-07 09:46:05 +0200

    Toggles dark mode on click

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.tabs.onUpdated.addListener(tabToWindow)
+chrome.action.onClicked.addListener(onClick)
 
 async function tabToWindow(id, { url }, { windowId }) {
   const { tab2window } = await chrome.storage.sync.get()
@@ -8,4 +9,17 @@ async function tabToWindow(id, { url }, { windowId }) {
   chrome.tabs.remove(id, () => {
     chrome.windows.create({ url, focused: true })
   })
+}
+
+function onClick(tab) {
+  chrome.scripting.executeScript({target: { tabId: tab.id }, func: toggle })
+}
+
+function toggle() {
+  const meta = document.head.querySelector('meta[name="color-scheme"]') || document.createElement('meta')
+  meta.name = 'color-scheme'
+  if (meta.content === 'only light') return meta.remove()
+
+  meta.content = 'only light'
+  document.head.appendChild(meta)
 }
