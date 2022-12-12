1857be165fa62caa0766cf35fa9244b9bafc776f
1miracle <dz.art.23@gmail.com>
2022-12-12 15:38:08 +0200

    Fix YouTube slider, Use chrome store for saving block list urls

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
 chrome.tabs.onUpdated.addListener(tabToWindow)
 chrome.action.onClicked.addListener(onClick)
 
+const checkBlockList = async (tabId, changeInfo, tab) => {
+  const { blockListUrls } = await chrome.storage.sync.get();
+
+  const url = new URL(tab.url);
+
+  if (blockListUrls?.includes(url.hostname)) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: toggle,
+    });
+  }
+};
+
+chrome.tabs.onUpdated.addListener(checkBlockList);
+
 async function tabToWindow(id, { url }, { windowId }) {
   const { tab2window } = await chrome.storage.sync.get()
   if (!tab2window || !url) return
@@ -11,8 +26,28 @@ async function tabToWindow(id, { url }, { windowId }) {
   })
 }
 
-function onClick(tab) {
-  chrome.scripting.executeScript({target: { tabId: tab.id }, func: toggle })
+async function onClick(tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: toggle,
+  });
+
+  let urls;
+  const url = new URL(tab.url);
+
+  chrome.storage.sync.get().then((result) => {
+    const { blockListUrls } = result;
+
+    if (!blockListUrls?.length) {
+      urls = [url.hostname];
+    } else if (blockListUrls.includes(url.hostname)) {
+      urls = blockListUrls.filter((v) => v != url.hostname);
+    } else {
+      urls = blockListUrls.concat(url.hostname);
+    }
+
+    chrome.storage.sync.set({ blockListUrls: urls });
+  });
 }
 
 function toggle() {
--- dark.js
+++ dark.js
@@ -1,14 +1,24 @@
+const CLASSES = {
+  volumeSliderHandle: ".ytp-volume-slider-handle"
+};
+
+const setSliderFilter = () => {
+  const slider = document.querySelector(CLASSES.volumeSliderHandle);
+  if (slider) slider.style.filter = "contrast(0.1) brightness(2)";
+};
+
 const setTheme = () => {
   const meta = document.head.querySelector('meta[name="theme-color"]') || document.createElement('meta')
   meta.name = 'theme-color'
   meta.content = '#000000'
   document.head.appendChild(meta)
+  setSliderFilter()
 }
 
 setTheme()
 setTimeout(setTheme, 100)
-document.addEventListener('DOMContentLoaded', setTheme, false)
 
+document.addEventListener('DOMContentLoaded', setTheme, false)
 document.addEventListener('DOMSubtreeModified', (e) => {
   const meta = document.head.querySelector('meta[name="theme-color"]')
   if (meta?.content === '#000000') return
