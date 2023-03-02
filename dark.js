ced96911ba1fb92bd780b4a45ebcd05b45713eed
galulex <galulex@gmail.com>
2023-03-02 22:34:47 +0200

    Fixes youtube slider with css

--- dark.js
+++ dark.js
@@ -1,18 +1,8 @@
-const CLASSES = {
-  volumeSliderHandle: ".ytp-volume-slider-handle"
-};
-
-const setSliderFilter = () => {
-  const slider = document.querySelector(CLASSES.volumeSliderHandle);
-  if (slider) slider.style.filter = "contrast(0.1) brightness(2)";
-};
-
 const setTheme = () => {
   const meta = document.head.querySelector('meta[name="theme-color"]') || document.createElement('meta')
   meta.name = 'theme-color'
   meta.content = '#000000'
   document.head.appendChild(meta)
-  setSliderFilter()
 
   chrome.storage.sync.get({ contrast: '0.4' }, ({ contrast }) => {
     document.documentElement.style.setProperty('--blackout-contrast', 1 + +(contrast))
