ee9d2f960e083533866e945e6a16d09048e9d910
galulex <galulex@gmail.com>
2024-01-29 10:07:24 +0200

    Fixes DevTools deep dark mode

--- devtools.js
+++ devtools.js
@@ -5,4 +5,11 @@ chrome.devtools.panels.applyStyleSheet(`:root {
   --color-background-elevation-1: #000 !important;
   --network-grid-default-color: #000 !important;
   --network-grid-stripe-color: #000 !important;
+  --sys-color-cdt-base-container: #000 !important;
+  --sys-color-cdt-base: #000 !important;
+  --sys-color-divider: #000 !important;
+  --sys-color-surface1: #000 !important;
+  --sys-color-surface2: #000 !important;
+  --color-grid-stripe: #000 !important;
+  --color-grid-default: #000 !important;
 } `);
